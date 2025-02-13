a2e6897c0a9f52a956dd581f6a17da1e363a4e10
Wanderson notebook <wandersonlife7@gmail.com>
2025-02-12 21:15:10 -0300

    creating Home page

---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import Navbar from "../components/Navbar";
+
+const Home = () => {
+  return (
+    <div>
+      <Navbar />
+      <h1>Clothing brand.</h1>
+    </div>
+  );
+};
+
+export default Home;
